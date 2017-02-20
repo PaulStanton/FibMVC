b0665de705a5dfe3acdd0c5c795f9aac49259a83
pstan <pstan@DESKTOP-F28K8BC>
2017-02-20 10:54:57 -0500

    Created TestVIew with TestModel to display first 15 FIbonacci Numbers.

---
+++ FibMVC.Biz/Biz.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibMVC.Biz
+{
+    public static class Biz
+    {
+        /// <summary>
+        /// starts with 0 
+        /// </summary>
+        /// <param name="numFib"></param>
+        /// <returns></returns>
+        public static List<int> FibonacciCalculator( int numFib)
+        {
+            List<int> tempFib = new List<int>();
+            int fibNum = 0;
+            if (numFib == 0)
+            {
+                return null;
+            }
+            if (numFib >= 1)
+            {
+                tempFib.Add(fibNum);
+                fibNum++;
+            }
+            if (numFib >= 2)
+            {
+                tempFib.Add(fibNum);
+            }
+            if (numFib >=3)
+            {
+                for (int i = 2; i < numFib; i++)
+                {
+                    fibNum = tempFib[i - 2] + tempFib[i - 1];
+                    tempFib.Add(fibNum);
+                }
+            }
+            return tempFib;
+
+            
+          
+            }
+        }
+    }
+
---
+++ FibMVC/Models/TestModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FibMVC.Biz;
+
+namespace FibMVC.Models
+{
+    public class TestModel
+    {
+        public List<int> fibNumbers = new List<int>();
+        public TestModel()
+        {
+
+        }
+        
+    }
+}
